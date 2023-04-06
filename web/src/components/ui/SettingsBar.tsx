0f3addcaf3cc1341b660a57c59dcc4e5822b01dd
Ivan Arsenev <42762155+ancill@users.noreply.github.com>
2023-04-06 13:02:16 -0300

    Implemented theme switching

--- web/src/components/ui/SettingsBar.tsx
+++ web/src/components/ui/SettingsBar.tsx
@@ -1,51 +1,66 @@
+import { useEffect, useState } from 'react'
+import { WiDaySunny, WiNightClear } from 'react-icons/wi'
+const ThmemeSwitch = () => {
+  const [theme, setTheme] = useState('light')
+  const toggleTheme = () => {
+    setTheme(theme === 'dark' ? 'light' : 'dark')
+  }
+  // initially set the theme and "listen" for changes to apply them to the HTML tag
+  useEffect(() => {
+    document.querySelector('html')?.setAttribute('data-theme', theme)
+  }, [theme])
+  return (
+    <label className='swap swap-rotate'>
+      <input onClick={toggleTheme} type='checkbox' />
+      <WiNightClear
+        data-set-theme='dark'
+        data-act-class='ACTIVECLASS'
+        className='swap-on fill-current w-6 h-6'
+      />
+      <WiDaySunny
+        data-set-theme='light'
+        data-act-class='ACTIVECLASS'
+        className='swap-off fill-current w-6 h-6'
+      />
+    </label>
+  )
+}
+const Profile = () => {
+  return (
+    <div className='dropdown dropdown-end'>
+      <label tabIndex={0} className='btn btn-ghost btn-circle avatar'>
+        <div className='w-10 rounded-full'>
+          <img src='https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg' />
+        </div>
+      </label>
+      <ul
+        tabIndex={0}
+        className='mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52'
+      >
+        <li>
+          <a className='justify-between'>
+            Profile
+            <span className='badge'>New</span>
+          </a>
+        </li>
+        <li>
+          <a>Settings</a>
+        </li>
+        <li>
+          <a>Logout</a>
+        </li>
+      </ul>
+    </div>
+  )
+}
 const SettingsBar = () => {
   return (
     <div className='flex justify-start items-start align-middle h-screen w-fit bg-base-300 flex-col'>
-      <div className='flex-1'>
-        <a className='btn btn-ghost normal-case text-xl'>TH</a>
+      <div className='flex-1 my-2 mx-1'>
+        <Profile />
       </div>
-      <div className='flex-none self-center'>
-        <div className='dropdown dropdown-end'>
-          <label tabIndex={0} className='btn btn-ghost btn-circle avatar'>
-            <div className='w-10 rounded-full'>
-              <img src='https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg' />
-            </div>
-          </label>
-          <ul
-            tabIndex={0}
-            className='mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52'
-          >
-            <li>
-              <a className='justify-between'>
-                Profile
-                <span className='badge'>New</span>
-              </a>
-            </li>
-            <li>
-              <a>Settings</a>
-            </li>
-            <li>
-              <a>Logout</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-      <div className='flex-none self-center'>
-        <button className='btn btn-square btn-ghost'>
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            fill='none'
-            viewBox='0 0 24 24'
-            className='inline-block w-5 h-5 stroke-current'
-          >
-            <path
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              strokeWidth='2'
-              d='M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z'
-            ></path>
-          </svg>
-        </button>
+      <div className='flex-none self-center flex flex-col mb-2'>
+        <ThmemeSwitch />
       </div>
     </div>
   )
--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,7 +1,12 @@
+import { useEffect } from 'react'
+import { themeChange } from 'theme-change'
 import SettingsBar from '../../components/ui/SettingsBar'
 import WorkSpaceBar from '../../components/ui/WorkSpaceBar'
-
 const Home = () => {
+  useEffect(() => {
+    themeChange(false)
+    // 👆 false parameter is required for react project
+  }, [])
   return (
     <div className='flex h-full'>
       <SettingsBar />
